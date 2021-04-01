c7e2fd381107beb7588239ae72b6a453527464fa
Dmitry Pavlov <perr07@yandex.ru>
2021-04-01 21:15:43 +0500

    Initial Commit

---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Pandos on 01.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
